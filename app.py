727ca3373ffd0e91fdb88f7da078db1756fee674
shuvo2085 <shuvobis085@gmail.com>
2026-05-22 15:15:24 +0530

    fix port for render deployment

--- app.py
+++ app.py
@@ -210,4 +210,5 @@ def on_chat_message(data):
     }, to=data["room_id"])
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
